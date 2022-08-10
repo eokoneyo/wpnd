d0bd8ec3c7421474fa5c22d118f7aaa44ac664bb
Eyo Okon Eyo <maziey93@gmail.com>
2022-08-11 01:01:05 +0200

    fix: modify entrypoint script file to accomodate work for composer

--- src/templates/core/entrypoint-child.sh
+++ src/templates/core/entrypoint-child.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 set -euo pipefail
 
-# Explicitly define the files that get symlinked from wpnd directory
+# Explicitly define the directories that get symlinked from wpnd directory
 # into wordpress installation directory
+allowed_directories=("themes" "plugins" "mu-plugins")
 
-for dir in themes plugins mu-plugins
-do
-  for f in "/usr/src/wpnd/$dir"/*;
-  do
-    mount_path="/usr/src/wordpress/wp-content/$dir/$(basename "$f")"
-    mount_path_parent_dir=$(dirname "$mount_path")
+for record in "/usr/src/wpnd"/*; do
+  if [[ "${allowed_directories[*]}" =~ ${record} ]]; then
+    # whatever you want to do when array contains value
+    dir=$record
+    for f in "/usr/src/wpnd/$dir"/*;
+    do
+      mount_path="/usr/src/wordpress/wp-content/$dir/$(basename "$f")"
+      mount_path_parent_dir=$(dirname "$mount_path")
 
-    # for files that will be mounted to mu-plugins directory confirm that theres neither a file or directory already pre-existing
-    if [[ $dir = mu-plugins ]] && ! [[ -d "$mount_path" || -f "$mount_path" ]]; then
+      # for files that will be mounted to mu-plugins directory confirm that theres neither a file or directory already pre-existing
+      if [[ $dir = mu-plugins ]] && ! [[ -d "$mount_path" || -f "$mount_path" ]]; then
 
-     # cover case where parent directory doesn't exist; i.e mu-plugins
-      if [[ ! -d "$mount_path_parent_dir" ]]; then
-        mkdir "$mount_path_parent_dir"
+        # cover case where parent directory doesn't exist; i.e mu-plugins
+        if [[ ! -d "$mount_path_parent_dir" ]]; then
+          mkdir "$mount_path_parent_dir"
+        fi
+
+        echo "Linking $(basename "$f") to $mount_path_parent_dir"
+
+        ln -s "$f" "$mount_path_parent_dir"
+
+      elif [[ -d "$f" && ! -d "$mount_path" && ! "$dir" = mu-plugins ]]; then
+
+        echo "Linking $(basename "$f") to $mount_path_parent_dir"
+
+        ln -s "$f" "$mount_path_parent_dir"
       fi
-
-      echo "Linking $(basename "$f") to $mount_path_parent_dir"
-
-      ln -s "$f" "$mount_path_parent_dir";
-
-    elif [[ -d "$f" && ! -d "$mount_path" && ! "$dir" = mu-plugins ]]; then
-
-      echo "Linking $(basename "$f") to $mount_path_parent_dir"
-
-      ln -s "$f" "$mount_path_parent_dir";
-    fi
-  done;
+    done
+  elif [[ -f "$record" && ! -f "$record" ]]; then
+    #TODO: decide how single files will be handled
+    echo "found $record"
+  fi
 done
 
 exec docker-entrypoint.sh "$@"
