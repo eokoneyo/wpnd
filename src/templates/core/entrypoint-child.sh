786839f71b6b3a5be6c0666612e864e29d33c239
Eyo Okon Eyo <maziey93@gmail.com>
2022-08-16 23:23:09 +0200

    fix: improve script to symlink files from wpnd directory into docker wp-content directory

--- src/templates/core/entrypoint-child.sh
+++ src/templates/core/entrypoint-child.sh
@@ -4,18 +4,17 @@ set -euo pipefail
 # Explicitly define the directories that get symlinked from wpnd directory
 # into wordpress installation directory
 allowed_directories=("themes" "plugins" "mu-plugins")
+docker_wp_content_path="/usr/src/wordpress/wp-content"
 
 for record in "/usr/src/wpnd"/*; do
-  if [[ "${allowed_directories[*]}" =~ ${record} ]]; then
-    # whatever you want to do when array contains value
-    dir=$record
-    for f in "/usr/src/wpnd/$dir"/*;
-    do
-      mount_path="/usr/src/wordpress/wp-content/$dir/$(basename "$f")"
+  record_name=$(basename "$record")
+  if [[ "${allowed_directories[*]}" =~ $record_name ]]; then
+    for f in "/usr/src/wpnd/$record_name"/*; do
+      mount_path="$docker_wp_content_path/$record_name/$(basename "$f")"
       mount_path_parent_dir=$(dirname "$mount_path")
 
       # for files that will be mounted to mu-plugins directory confirm that theres neither a file or directory already pre-existing
-      if [[ $dir = mu-plugins ]] && ! [[ -d "$mount_path" || -f "$mount_path" ]]; then
+      if [[ $record_name = "mu-plugins" ]] && ! [[ -d "$mount_path" || -f "$mount_path" ]]; then
 
         # cover case where parent directory doesn't exist; i.e mu-plugins
         if [[ ! -d "$mount_path_parent_dir" ]]; then
@@ -26,16 +25,16 @@ for record in "/usr/src/wpnd"/*; do
 
         ln -s "$f" "$mount_path_parent_dir"
 
-      elif [[ -d "$f" && ! -d "$mount_path" && ! "$dir" = mu-plugins ]]; then
+      elif [[ -d "$f" && ! -d "$mount_path" && ! "$record_name" = "mu-plugins" ]]; then
 
         echo "Linking $(basename "$f") to $mount_path_parent_dir"
 
         ln -s "$f" "$mount_path_parent_dir"
       fi
     done
-  elif [[ -f "$record" && ! -f "$record" ]]; then
-    #TODO: decide how single files will be handled
-    echo "found $record"
+  elif [[ -f "$record" && ! -f "$docker_wp_content_path/$record_name" ]]; then
+    echo "linking $record_name to $docker_wp_content_path"
+    ln -s "$record" "$docker_wp_content_path"
   fi
 done
 
