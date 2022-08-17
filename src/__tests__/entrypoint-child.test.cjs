164b973c59417b928ad1f75b783a4e2ab7e2c1eb
Eyo Okon Eyo <maziey93@gmail.com>
2022-08-17 02:30:20 +0200

    chore: add scaffold test for entrypoint child script

---
+++ src/__tests__/entrypoint-child.test.cjs
@@ -0,0 +1,5 @@
+describe('entrypoint child script', () => {
+  it.todo('copies over only the defined allowed directories into the specified wordpress wp-content installation path');
+
+  it.todo('copies over every file in the wpnd directory into the specified wordpress wp-content installation path');
+});
