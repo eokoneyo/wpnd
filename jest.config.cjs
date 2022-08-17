59b1281931c10e321bed5948d6af2a0597de6f4a
Eyo Okon Eyo <maziey93@gmail.com>
2022-08-17 02:10:42 +0200

    chore: add setup for tests

---
+++ jest.config.cjs
@@ -0,0 +1,13 @@
+const pkg = require('./package.json');
+
+module.exports = {
+  displayName: pkg.name,
+  testMatch: ["**/__tests__/**/*.?(c)[jt]s", "**/?(*.)+(spec|test).?(c)[jt]s"],
+  testPathIgnorePatterns: [
+    '<rootDir>/src/__tests__/setup.cjs',
+    '<rootDir>/src/__tests__/__mocks__',
+    '<rootDir>/src/__tests__/helpers',
+  ],
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.cjs'],
+};
