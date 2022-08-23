9e366a2c04ed2f68794e8b61903eb9d06160e8fb
Eyo Okon Eyo <maziey93@gmail.com>
2022-08-23 23:36:45 +0200

    chore: cleanup setup for jest tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,6 +3,9 @@ const pkg = require('./package.json');
 module.exports = {
   displayName: pkg.name,
   testMatch: ["**/__tests__/**/*.?(c)[jt]s", "**/?(*.)+(spec|test).?(c)[jt]s"],
+  transform: {
+    '\\.js': 'jest-esm-transformer'
+  },
   testPathIgnorePatterns: [
     '<rootDir>/src/__tests__/setup.cjs',
     '<rootDir>/src/__tests__/__mocks__',
