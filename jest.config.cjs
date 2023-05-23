f4ab1d95337ccf9e97ed883baed36282c8fcf891
Eyo Okon Eyo <maziey93@gmail.com>
2023-05-23 22:33:24 +0200

    chore: move test files into src directory

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,6 +3,7 @@ const pkg = require('./package.json');
 /** @type {import('jest').Config} */
 const config = {
   displayName: pkg.name,
+  rootDir: "src",
   testMatch: ["**/__tests__/**/*.?(c)[jt]s", "**/?(*.)+(spec|test).?(c)[jt]s"],
   transform: {},
   testPathIgnorePatterns: [
